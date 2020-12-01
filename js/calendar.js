6ef94184e21971d94200227cd2e961a74e46364f
PilzHere <contact@christianpilz.com>
2020-12-01 11:30:40 +0100

    A grid with dates, display of month an a textaera added.

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,27 @@
 "use_strict";
 
-alert("Great Scott!!!");
+const monthNames = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
+
+const today = new Date();
+
+function getCurrentTime() {
+	const fullDate = today.toISOString().slice(0, 10);
+
+	document.getElementById("selectedMonth").innerHTML =
+		monthNames[today.getMonth()] + " " + today.getFullYear();
+}
+
+getCurrentTime();
